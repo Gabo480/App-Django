575adbd5d4caf8a4fd77da1e88ed7ac3ec46cbd1
Gabo480 <gabriel.cortes.martinez17@gmail.com>
2023-10-12 22:04:37 -0600

    primero subida

---
+++ ia01/ia01/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ia01 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ia01.settings')
+
+application = get_wsgi_application()
